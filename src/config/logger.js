62a7119840f8679ab091bdceedb6fb42b69f9377
Shubham Taywade <shubhamtaywade82@gmail.com>
2025-06-15 22:02:05 +0530

    Update logger.js

--- src/config/logger.js
+++ src/config/logger.js
@@ -5,11 +5,20 @@ module.exports = createLogger({
   format: format.combine(
     format.colorize(),
     format.timestamp({ format: "YYYY-MM-DD HH:mm:ss.SSS" }),
-    format.printf(
-      ({ timestamp, level, message, ...meta }) =>
-        `${timestamp} ${level}: ${message}` +
-        (Object.keys(meta).length ? " " + JSON.stringify(meta) : "")
-    )
-  ),
+    //format.printf(
+     // ({ timestamp, level, message, ...meta }) =>
+      //  `${timestamp} ${level}: ${message}` +
+     //   (Object.keys(meta).length ? " " + JSON.stringify(meta) : "")
+   // )
+format.printf(({ timestamp, level, message, ...meta }) => {
+  let metaStr = '';
+  try {
+    metaStr = Object.keys(meta).length ? ' ' + JSON.stringify(meta) : '';
+  } catch (e) {
+    metaStr = ' (meta: circular)';
+  }
+  return `${timestamp} ${level}: ${message}${metaStr}`;
+})
+ // ),
   transports: [new transports.Console()],
 });
